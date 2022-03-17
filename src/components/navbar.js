b9bfad81c1432b388346c7b2e3cb034ec7ca96ec
Laura You-Li Chen <lauraylchen@hotmail.com>
2022-03-17 15:34:45 -0400

    Changed the image and display it in the navbar

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,27 +1,28 @@
 import React from 'react'
+import logo from '../images/logo.png'
 
 export default function Navbar() {
   return (
     <section id="navbar" className="navbar navbar-expand-sm">
       <div className="container justify-content-center">
         <div className="collapse navbar-collapse">
-          <a class="navbar-brand" href="#header">
-            <img src="../images/logo.png" alt="Laura's Logo" />
+          <a className="navbar-brand" href="#header">
+            <img src={logo} alt="Laura's logo"/>
           </a>
         </div>
         <div className="navbar-links">
-          <ul class="navbar-nav">
-            <li class="nav-item">
-              <a class="nav-link" href="#about">About</a>
+          <ul className="navbar-nav">
+            <li className="nav-item">
+              <a className="nav-link" href="#about">About</a>
             </li>
-            <li class="nav-item">
-              <a class="nav-link" href="#">Technologies</a>
+            <li className="nav-item">
+              <a className="nav-link" href="#technologies">Technologies</a>
             </li>
-            <li class="nav-item">
-              <a class="nav-link" href="#">Projects</a>
+            <li className="nav-item">
+              <a className="nav-link" href="#projects">Projects</a>
             </li>
-            <li class="nav-item">
-              <a class="nav-link" href="#">Contact</a>
+            <li className="nav-item">
+              <a className="nav-link" href="#contact">Contact</a>
             </li>
           </ul>
         </div>
