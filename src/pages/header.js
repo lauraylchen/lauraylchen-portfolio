f012663c01f63ae7101bfd97334a632b547df017
Laura You-Li Chen <lauraylchen@hotmail.com>
2022-03-17 17:16:05 -0400

    Finish design of banner

--- src/pages/header.js
+++ src/pages/header.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import Navbar from "../components/navbar"
+import portrait from "../images/portrait.png"
 
 export default function Header() {
   return (
@@ -8,7 +9,14 @@ export default function Header() {
 
       <div id="banner">
         <div className="container">
-          <h3>Hi, I am Laura</h3>
+          <h2>Hi, I am Laura</h2>
+          <div className='d-flex justify-content-center'>
+            <div className='pink-portrait d-flex justify-content-center'>
+              <h3>FULL-STACK</h3>
+              <h1>DEVELOPER</h1>
+              <img src={portrait} alt="Portrait of Laura"/>
+            </div>
+          </div>
         </div>
       </div>
     </div>
