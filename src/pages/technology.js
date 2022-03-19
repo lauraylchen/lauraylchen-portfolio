ad94b6c5bb39f43c52d7f58d3a81044d87188a5b
Laura You-Li Chen <lauraylchen@hotmail.com>
2022-03-18 22:06:11 -0400

    Added responsive to technology cards

--- src/pages/technology.js
+++ src/pages/technology.js
@@ -5,14 +5,14 @@ export default function Technology() {
     <div id="technology">
       <div className="container">
         <h2>TOOLS THAT I USE</h2>
-        <div className="d-flex justify-content-around">
-          <div className="backend">
+        <div className="row">
+          <div className="col-12 col-lg-4 backend">
             <h3>Back-end</h3>
           </div>
-          <div className="frontend">
+          <div className="col-12 col-lg-4 frontend">
             <h3>Front-end</h3>
           </div>
-          <div className="others">
+          <div className="col-12 col-lg-4 others">
             <h3>Other Tools and API</h3>
           </div>
         </div>
