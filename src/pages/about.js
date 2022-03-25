d843e5f421c842b17231dce4eb7afbc981899341
Laura You-Li Chen <lauraylchen@hotmail.com>
2022-03-25 14:26:53 -0400

    Changed the colors of my portfolio

--- src/pages/about.js
+++ src/pages/about.js
@@ -7,7 +7,7 @@ export default function About() {
       <div className='about-text'>
         <h2>WHO AM I</h2>
         <h1>Laura You-Li Chen</h1>
-        <h3>Full Stack Developer</h3>
+        <h3>FULL-STACK DEVELOPER</h3>
         <p>
           Newly graduated from Le Wagon bootcamp, I chose web development because
           it requires technology, art and design all at once, which is what I am
