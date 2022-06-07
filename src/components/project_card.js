636facdcfc3c1bbd5565f86ee996cef6c62f3db8
Laura You-Li Chen <lauraylchen@hotmail.com>
2022-06-07 16:56:40 -0400

    Added stylepage for component project card and basic css

---
+++ src/components/project_card.js
@@ -0,0 +1,8 @@
+import React from 'react'
+
+export default function ProjectCard() {
+  return (
+    <div>
+    </div>
+  )
+}
--- src/pages/projects.js
+++ src/pages/projects.js
@@ -1,9 +1,12 @@
 import React from "react";
+import ProjectCard from "../components/project_card"
 
 export default function Projects() {
   return (
     <div id="projects">
       <div className="container">
+        <h2>RECENT PROJECTS</h2>
+        <ProjectCard />
       </div>
     </div>
   )
