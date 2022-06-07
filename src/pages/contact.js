68cb5a75bb5cbb815c4e7757089a23b12e10a91a
Laura You-Li Chen <lauraylchen@hotmail.com>
2022-06-07 17:16:19 -0400

    Added style and content to contact section

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -2,8 +2,10 @@ import React from "react";
 
 export default function Contact() {
   return (
-    <div id="contact">
-      <div className="container">
+    <div id="contact" className="container">
+      <div className="contact-text">
+        <h2>CONTACT ME!</h2>
+        <h3>Send me a message and I'll get in touch shortly, or feel free to reach out on Twitter/Linkedin</h3>
       </div>
     </div>
   )
