aea9ee7ac07499360655e44f85c8e5a360b8f8ca
Laura You-Li Chen <lauraylchen@hotmail.com>
2022-06-08 22:06:55 -0400

    Fixed contact section with responsive for button and icons

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -8,12 +8,12 @@ export default function Contact() {
       <div className="contact-text">
         <h2>CONTACT ME!</h2>
         <h3>Send me a message and I'll get in touch shortly, or feel free to reach out on Twitter/Linkedin</h3>
-        <div className="d-flex justify-content-between pt-5">
+        <div className="d-flex justify-content-between pt-5 flex-wrap">
           <a href="mailto:lauraylchen@hotmail.com" className="btn-white">lauraylchen@hotmail.com</a>
-          <div>
-            <a href="https://github.com/lauraylchen/" target="_blank" className="fa-icons"><FaGithub /></a>
-            <a href="https://twitter.com/lauraylchen/" target="_blank" className="fa-icons"><FaTwitter /></a>
-            <a href="https://www.linkedin.com/in/lauraylchen/" target="_blank" className="fa-icons"><FaLinkedin /></a>
+          <div className="icons">
+            <a href="https://github.com/lauraylchen/" target="_blank" className="fa-icon"><FaGithub /></a>
+            <a href="https://twitter.com/lauraylchen/" target="_blank" className="fa-icon"><FaTwitter /></a>
+            <a href="https://www.linkedin.com/in/lauraylchen/" target="_blank" className="fa-icon"><FaLinkedin /></a>
           </div>
         </div>
       </div>
