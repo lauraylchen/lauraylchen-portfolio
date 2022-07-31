e5f1e151db3cbf2b7ddc8765ada9667fe7566348
Laura You-Li Chen <lauraylchen@hotmail.com>
2022-07-31 14:05:30 -0400

    Update completed projects. Added myMoney

--- src/components/project_card.js
+++ src/components/project_card.js
@@ -3,6 +3,7 @@ import seamify_landing_page from "../images/projects_screenshots/seamify_landing
 import cooking_ninja_landing_page from "../images/projects_screenshots/cooking_ninja_landing_page.png";
 import sunnyside_landing_page from "../images/projects_screenshots/sunnyside_landing_page.png";
 import pokemon_match_landing_page from "../images/projects_screenshots/pokemon_match_landing_page.png";
+import mymoney_landing_page from "../images/projects_screenshots/mymoney_landing_page.png";
 
 
 export default function ProjectCard() {
@@ -99,6 +100,52 @@ export default function ProjectCard() {
               </div>
             </div>
           </div>
+        </div>
+      </div>
+      <div className='row'>
+        <div className='col-12 col-lg-6 px-4'>
+          <div className='project-card card-left'>
+            <div className='landing-image'>
+              <a href="https://mymoney-financetracker.firebaseapp.com" target="_blank">
+                <img src={mymoney_landing_page} alt="Landing page of myMoney Finance Tracker" />
+              </a>
+            </div>
+            <div className='p-4 d-flex flex-column justify-content-between web-info'>
+              <h3>myMoney</h3>
+              <p className='web-about'>
+                myMoney is a finance tracker. Login to test: ( email: laura@mymoney.com / password: test123456 )
+              </p>
+              <p className='tool-used'>
+                React | HTML | CSS | JS | FireBase | Firestore
+              </p>
+              <div className='d-flex justify-content-center'>
+                <a href='https://mymoney-financetracker.firebaseapp.com' target="_blank" className='btn-brown'>VISIT</a>
+                <a href='https://github.com/lauraylchen/my-money' target="_blank" className='btn-brown'>CODE</a>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className='col-12 col-lg-6 px-4'>
+          {/*<div className='project-card'>
+            <div className='landing-image'>
+              <a href="https://pokemon-match-cards.netlify.app" target="_blank">
+                <img src={pokemon_match_landing_page} alt="Landing page of Pokemon Match" />
+              </a>
+            </div>
+            <div className='p-4 d-flex flex-column justify-content-between web-info'>
+              <h3>Pokemon Match</h3>
+              <p className='web-about'>
+                Matching card game with Pokemon theme.
+              </p>
+              <p className='tool-used'>
+                React | HTML | CSS | JS | TailwindCSS | Daisy UI | Netlify
+              </p>
+              <div className='d-flex justify-content-center'>
+                <a href='https://pokemon-match-cards.netlify.app' target="_blank" className='btn-brown'>VISIT</a>
+                <a href='https://github.com/lauraylchen/magic-memory' target="_blank" className='btn-brown'>CODE</a>
+              </div>
+            </div>
+          </div> */}
         </div>
       </div>
     </div>
