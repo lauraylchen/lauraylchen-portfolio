08d6c012a3ed6168386434e8a125a95fbf4c703d
Laura You-Li Chen <lauraylchen@hotmail.com>
2022-08-12 12:43:40 -0400

    Fix typo about page

--- src/pages/about.js
+++ src/pages/about.js
@@ -16,7 +16,7 @@ export default function About() {
           Newly graduated from Le Wagon bootcamp, I chose web development because
           it requires technology, art and design all at once, which is what I am
           passionate about. At the same time, I strongly believe that any project
-          could be develop with teamwork and strong communication.
+          could be developed with teamwork and strong communication.
         </p>
         <br />
         <p>
